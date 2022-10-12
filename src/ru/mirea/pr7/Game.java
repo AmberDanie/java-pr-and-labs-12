40a1906b746224288d4a06b4639e2852b8760f88
AmberDanie <91751423+AmberDanie@users.noreply.github.com>
2022-10-12 22:51:52 +0300

    паузу надо

--- src/ru/mirea/pr7/Game.java
+++ src/ru/mirea/pr7/Game.java
@@ -2,105 +2,163 @@ package ru.mirea.pr7;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.Stack;
 
 public class Game extends JFrame {
+    private final JFrame frame = new JFrame("Drunker");
     private final JPanel centerPanel = new JPanel(new GridLayout(2, 1));
-    private final JLabel northPanel = new JLabel("Choose 5 cards from these:");
-    private final Stack<Game.Card> cards = new Stack<Game.Card>();
-    private final Stack<Game.Card> firstPlayerCards = new Stack<Game.Card>();
-    private final Stack<Game.Card> secondPlayerCards = new Stack<Game.Card>();
+    private final JPanel northPanel = new JPanel(new GridLayout(1, 10));
+    private final JPanel southPanel = new JPanel(new GridLayout(1, 10));
+    private final JLabel northLabel = new JLabel("Choose 5 cards from these:");
+    private final JLabel centerLabel = new JLabel("");
+    private final Stack<Game.Card> cards = new Stack<>();
+    private final Stack<Game.Card> firstPlayerCards = new Stack<>();
+    private final Stack<Game.Card> secondPlayerCards = new Stack<>();
     private class Card {
+        private static int size = 256;
         private static int countOfChosenCards = 0;
         private final int cardId;
         private final JLabel imageLabel;
+        private final ImageIcon imgIcon;
 
-        public int getCardId() {
-            return cardId;
-        }
+        private final MouseListener ma = new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                Image image = imgIcon.getImage(); // изменяем размеры картинки под 256х256 для удобства
+                Image newImg = image.getScaledInstance(size+(size/16), size+(size/16),  java.awt.Image.SCALE_SMOOTH);
+                imageLabel.setIcon(new ImageIcon(newImg));
+            }
 
-        public static int getCountOfChosenCards() {
-            return countOfChosenCards;
-        }
+            @Override
+            public void mouseExited(MouseEvent e) {
+                Image image = imgIcon.getImage(); // изменяем размеры картинки под 256х256 для удобства
+                Image newImg = image.getScaledInstance(size, size,  java.awt.Image.SCALE_SMOOTH);
+                imageLabel.setIcon(new ImageIcon(newImg));
+            }
+        };
 
-        public JLabel getImageLabel() {
-            return imageLabel;
-        }
+        private final MouseListener ml = new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                countOfChosenCards++;
+                imageLabel.setVisible(false);
+                northLabel.setText("You chose card with number " + cardId + ". " +
+                        (5 - countOfChosenCards) + " cards left to choose");
+                firstPlayerCards.add(Card.this);
+                cards.remove(Card.this);
+                if (countOfChosenCards == 5) {
+                    while (cards.size() != 0) {
+                        Card card = cards.firstElement();
+                        secondPlayerCards.add(card);
+                        card.imageLabel.setVisible(false);
+                        cards.remove(card);
+                    }
+                    for (int i = 0; i < 5; i++) {
+                        centerPanel.remove(firstPlayerCards.get(i).imageLabel);
+                        centerPanel.remove(secondPlayerCards.get(i).imageLabel);
+                    }
+                    removeMouseListener(this);
+                    System.out.println(middle());
+                }
+            }
+        };
 
-        public Card(String path, int id) {
+        public Card(int id) {
             this.cardId = id;
-            ImageIcon imgIcon = new ImageIcon(path);
+            imgIcon = new ImageIcon("src/ru/mirea/pr7/cards/"+id+".png");
             Image image = imgIcon.getImage(); // изменяем размеры картинки под 256х256 для удобства
             Image newImg = image.getScaledInstance(256, 256,  java.awt.Image.SCALE_SMOOTH);
             imageLabel = new JLabel(new ImageIcon(newImg));
             imageLabel.setOpaque(false);
-            imageLabel.addMouseListener(new MouseListener() {
-                @Override
-                public void mouseClicked(MouseEvent e) {
-                    if (countOfChosenCards >= 5)
-                        return;
-                    countOfChosenCards++;
-                    imageLabel.setVisible(false);
-                    northPanel.setText("You chose card with number " + cardId + ". " +
-                            (5 - countOfChosenCards) + " cards left to choose");
-                    // здесь кидать надо карту в список первого (firstPlayerCards)
-                    if (countOfChosenCards == 5) {
-                        while (cards.size() != 0) {
-                            Card card = cards.pop();
-                            card.getImageLabel().setVisible(false);
-                            secondPlayerCards.add(card);
-                        }
-                        for (Card firstPlayerCard : firstPlayerCards)
-                            System.out.print(firstPlayerCard.getCardId() + " ");
-                        System.out.println();
-                        for (Card secondPlayerCard : secondPlayerCards)
-                            System.out.print(secondPlayerCard.getCardId() + " ");
-                    }
-                }
-
-                @Override
-                public void mousePressed(MouseEvent e) {
-
-                }
-
-                @Override
-                public void mouseReleased(MouseEvent e) {
-
-                }
-
-                @Override
-                public void mouseEntered(MouseEvent e) {
-
-                }
-
-                @Override
-                public void mouseExited(MouseEvent e) {
-
-                }
-            });
-
+            imageLabel.addMouseListener(ma);
+            imageLabel.addMouseListener(ml);
         }
     }
+
+    public void prepareCards(Stack<Game.Card> cards, JPanel panel) {
+        for (Card card : cards) {
+            JLabel imageLabel = card.imageLabel;
+            Image image = card.imgIcon.getImage(); // изменяем размеры картинки под 256х256 для удобства
+            Image newImg = image.getScaledInstance(128, 128,  java.awt.Image.SCALE_SMOOTH);
+            Card.size = 128;
+            imageLabel.setIcon(new ImageIcon(newImg));
+            imageLabel.setVisible(true);
+            panel.add(imageLabel);
+            imageLabel.removeMouseListener(card.ml);
+        }
+    }
+
     public Game() {
+        frame.setLayout(new BorderLayout());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         opening();
     }
     public void opening() {
-        JFrame frame = new JFrame("Drunker");
-        frame.setLayout(new BorderLayout());
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         for (int i = 0; i < 10; i++) {
-            String path = "src/ru/mirea/pr7/cards/"+i+".png";
-            cards.add(new Card(path, i));
-            centerPanel.add(cards.lastElement().getImageLabel());
+            cards.add(new Card(i));
+            centerPanel.add(cards.lastElement().imageLabel);
         }
 
         frame.add(centerPanel, BorderLayout.CENTER);
-        frame.add(northPanel, BorderLayout.NORTH);
+        frame.add(northLabel, BorderLayout.NORTH);
 
         frame.pack();
         frame.setVisible(true);
+    }
+    public String middle()  {
+        // Раскладываем выбранные карты
+        prepareCards(firstPlayerCards, southPanel);
+        prepareCards(secondPlayerCards, northPanel);
+        //
+        frame.remove(northLabel);
+        frame.add(centerPanel, BorderLayout.CENTER);
+        frame.add(northPanel, BorderLayout.NORTH);
+        frame.add(southPanel, BorderLayout.SOUTH);
+        //
+        for (int i = 0; i < 106 && firstPlayerCards.size() != 0 &&
+                secondPlayerCards.size() != 0; i++) {
+            Card card1 = firstPlayerCards.pop();
+            Card card2 = secondPlayerCards.pop();
+            if ((card1.cardId == 0 && card2.cardId == 9) || (card1.cardId == 9 && card2.cardId == 0)) {
+                if (card1.cardId == 0) {
+                    southPanel.add(card2.imageLabel);
+                    firstPlayerCards.add(card1);
+                    firstPlayerCards.add(card2);
+                }
+                else {
+                    northPanel.add(card1.imageLabel);
+                    secondPlayerCards.add(card1);
+                    secondPlayerCards.add(card2);
+                }
+            }
+            else {
+                if (card1.cardId > card2.cardId) {
+                    southPanel.add(card2.imageLabel);
+                    firstPlayerCards.add(card1);
+                    firstPlayerCards.add(card2);
+                }
+                else {
+                    northPanel.add(card1.imageLabel);
+                    secondPlayerCards.add(card1);
+                    secondPlayerCards.add(card2);
+                }
+            }
+            if (i == 105) {
+                centerLabel.setText("Second");
+                return "botva";
+            }
+            // здесь надо паузу
+        }
+        if (firstPlayerCards.size() == 0) {
+            centerLabel.setText("Second");
+            centerPanel.add(centerLabel);
+            return "Second player is winner";
+        }
+        centerLabel.setText("First");
+        centerPanel.add(centerLabel);
+        return "First player is winner";
     }
 }
