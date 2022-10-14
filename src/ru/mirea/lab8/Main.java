005b5d99004387b8664ef0bacff159c35532a6be
AmberDanie <91751423+AmberDanie@users.noreply.github.com>
2022-10-14 19:46:54 +0300

    7lb+8lb

--- src/ru/mirea/lab8/Main.java
+++ src/ru/mirea/lab8/Main.java
@@ -1,7 +1,7 @@
 package ru.mirea.lab8;
 public class Main {
     public static void main(String[] args) {
-        String path="text.txt";
+        String path="src/ru/mirea/lab8/text.txt";
         MyFileReader readerFile = new MyFileReader();
         readerFile.read(path);
     }
